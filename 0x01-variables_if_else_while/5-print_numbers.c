d741349362d5ee7341cd4cbff0e62949b28e8064
ShariiphaMaltitiAhmad <mohammedsharifatu7@gmail.com>
2023-04-21 16:51:39 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,16 +4,15 @@
 
 /**
  * main - print 0 to 9
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0, num < 10; num++;)
-		printf("%d", num);
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
 
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
