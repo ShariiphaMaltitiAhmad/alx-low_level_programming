280e9e5ab97f4766b9aa0d7ad5db2971b1362171
ShariiphaMaltitiAhmad <mohammedsharifatu7@gmail.com>
2023-04-21 12:36:12 -0700

    alphabet soup without q n e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - lowercase in new line except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
