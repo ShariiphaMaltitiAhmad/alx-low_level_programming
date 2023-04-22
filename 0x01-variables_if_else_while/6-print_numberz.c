b9b20b684c6ed15f9a1d58f2ad5f883f864e94b7
ShariiphaMaltitiAhmad <mohammedsharifatu7@gmail.com>
2023-04-21 17:02:06 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -8,9 +10,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10, num++;)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	putchar('\n');
+
 	return (0);
 }
